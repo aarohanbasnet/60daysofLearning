5804e5b941e0fe17f9c096abef9e321ca63d860c
aarohanbasnet <letsmailaarohan@gmail.com>
2025-07-22 22:53:48 +0545

    Traffic Light SetUp

--- Project/my-react-app/src/TrafficLight.jsx
+++ Project/my-react-app/src/TrafficLight.jsx
@@ -2,9 +2,15 @@ function TrafficLight(){
     return(
         <>
         <div className="container">
-            <div className="red"></div>
+            <div className="lights">
+                 <div className="red"></div>
+            </div>
+           <div className="lights">
             <div className="yellow"></div>
+           </div>
+            <div className="lights">
             <div className="green"></div>
+            </div>
         </div>
         </>
     );
