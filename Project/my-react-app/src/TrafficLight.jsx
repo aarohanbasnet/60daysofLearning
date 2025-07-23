78610a8deeffc3309158da353fa5daec891eddca
aarohanbasnet <letsmailaarohan@gmail.com>
2025-07-23 16:32:40 +0545

    Day 53 -TrafficLight

--- Project/my-react-app/src/TrafficLight.jsx
+++ Project/my-react-app/src/TrafficLight.jsx
@@ -1,16 +1,63 @@
+import React, {useEffect, useState} from "react";
+
+const LIGHTS ={
+        red : 'red',
+        green : 'green',
+        yellow : 'yellow'
+    }
+
 function TrafficLight(){
+
+    const [active, setActvie] = useState(LIGHTS.yellow);
+    const [nextLight, setNextLight] = useState(LIGHTS.green);
+
+    //initialize timing
+
+    useEffect(()=>{
+             //either switch or  if else
+        let timeOutID;
+                switch(active) {
+                    case LIGHTS.red:
+                        timeOutID = setTimeout(()=>{
+                            setNextLight(LIGHTS.green);
+                            setActvie(LIGHTS.yellow);
+                            
+                        }, 4000)
+                        break;
+
+                         case LIGHTS.green:
+                         timeOutID =  setTimeout(()=>{
+                            setNextLight(LIGHTS.red);
+                                setActvie(LIGHTS.yellow);
+                            },3000)
+                            break;
+                
+                    
+                        case LIGHTS.yellow:
+                         timeOutID =  setTimeout(()=>{
+                                setActvie(nextLight);
+                            }, 500)
+                            break;
+
+                         
+                        default: break;
+    }       
+
+    return() => clearTimeout(timeOutID);
+         
+    }, [active]);
+
+    
     return(
         <>
+        <h1>Traffic Light</h1>
         <div className="container">
-            <div className="lights">
-                 <div className="red"></div>
-            </div>
-           <div className="lights">
-            <div className="yellow"></div>
-           </div>
-            <div className="lights">
-            <div className="green"></div>
-            </div>
+           <div className="light green"
+                style={active !== LIGHTS.green ? {opacity: .3} : null}></div>
+           <div className="light yellow"
+                style={active !== LIGHTS.yellow ? {opacity: .3} : null}></div>
+           <div className="light red"
+                style={active !== LIGHTS.red ? {opacity: .3} : null}></div>
         </div>
         </>
     );
