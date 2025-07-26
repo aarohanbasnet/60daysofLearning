31a3a45f73ebcfe8b5c2afb9d539e9eab020faf7
aarohanbasnet <letsmailaarohan@gmail.com>
2025-07-26 18:26:02 +0545

    Day 56 React Router

--- React-Router/src/Components/NavBar.jsx
+++ React-Router/src/Components/NavBar.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
 const NavBar = () => {
   return (
     <div className='navbar'>
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0A1EdYieSByo9yHxlkojqaBL_EnUqQg2OoTjyyG2SZvl4s1AvYx_7LgvvQMkyIGwQpDo&usqp=CAU" alt="router-logo" width="130px"/>
         <ul>
-            <Link to = "/"><li>Home</li></Link>
-            <Link to = "/products"><li>Products</li></Link>
-            <Link to = "/about"><li>About</li></Link>
-            <Link to = "/contact"><li>Contact</li></Link>
+            <NavLink to = "/"><li>Home</li></NavLink>
+            <NavLink to = "/products"><li>Products</li></NavLink>
+            <NavLink to = "/about"><li>About</li></NavLink>
+            <NavLink to = "/contact"><li>Contact</li></NavLink>
         </ul>
         <button>Get Started</button>
     </div>
