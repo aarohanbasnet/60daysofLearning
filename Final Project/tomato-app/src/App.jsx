ce4310b127b3aa3401a6e6945bc947a27eedec37
aarohanbasnet <letsmailaarohan@gmail.com>
2025-08-05 22:10:24 +0545

    Login Page setup

--- Final Project/tomato-app/src/App.jsx
+++ Final Project/tomato-app/src/App.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from './components/NavBar/NavBar'
 import { Route, Routes, Link} from 'react-router-dom'
 import Home from './Pages/Home/Home'
 import Cart from './Pages/Cart/Cart'
 import Order from './Pages/PlacedOrder/Order'
 import Footer from './components/Footer/Footer'
+import LoginPopup from './components/Login-popup/LoginPopup'
 const App = () => {
-  <Link to="/">Home Page</Link>
+  const [showLogin, setShowLogin] = useState(false);
   return (
     <>
+    {showLogin ? <LoginPopup/>: <></>}
     <div className='app'>
-      <NavBar/>
+      <NavBar setShowLogin = {setShowLogin}/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/cart' element={<Cart/>}/>
