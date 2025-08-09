8e20412259202621c400a441e0c841431b569e7e
aarohanbasnet <letsmailaarohan@gmail.com>
2025-08-09 23:04:57 +0545

    Sign Up feature

--- Final Project/tomato-app/src/components/Login-popup/LoginPopup.jsx
+++ Final Project/tomato-app/src/components/Login-popup/LoginPopup.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './LoginPopup.css'
 
 const LoginPopup = () => {
+
+  const [currState, setCurrState] = useState("Sign Up")
   return (
     <div className='loginPopup'>
-        <h1>Login</h1>
+        <form action="" className='login-popup-container'>
+          <div className="login-popup-title">
+            <h2></h2>
+          </div>
+        </form>
     </div>
   )
 }
